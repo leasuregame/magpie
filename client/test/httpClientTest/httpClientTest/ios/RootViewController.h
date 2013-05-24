8e24bea2bac690ea37504b9941c7f9aa7ff8f7da
linchenchen <linchenchen@leasuregame.com>
2013-05-24 10:29:47 +0800

    上传HttpClient测试C++版本
    上传HttpClient测试C++版本，封装好函数，供js调用

---
+++ client/test/httpClientTest/httpClientTest/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  httpClientTestAppController.h
+//  httpClientTest
+//
+//  Created by lCeve on 13-5-23.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end

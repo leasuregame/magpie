ed04929eb01585e7af6ffb95f597d14d75aec75c
wuzhanghai <wuzhanghai@leasuregame.com>
2013-07-22 08:41:20 -0700

    modify sync

--- game-server/src/app/dao/mysql/mapping/cardSync.js
+++ game-server/src/app/dao/mysql/mapping/cardSync.js
@@ -10,11 +10,7 @@
 /*
 * card sync
 * */
-
-
-var sqlHelper = require("../sqlHelper");
-var dbClient = require("pomelo").app.get("dbClient");
-var logger = require("pomelo-logger").getLogger(__filename);
+var CardDao = require('../cardDao');
 
 cardSync = {
     /*
@@ -33,22 +29,10 @@ cardSync = {
             return cb("param error", null);
         }
 
-        var stm = sqlHelper.updateSql("card", {"id": param.id}, param.data);
-        logger.debug(stm.sql, stm.args);
-        return dbClient.update(stm.sql, stm.args, function (err, res) {
-            if (err) {
-                logger.error("[cardDao.updateCardById faild] ", err.stack);
-
-                return cb({
-                    code: err.code,
-                    msg: err.message
-                }, null);
-            } if (!!res && res.affectedRows > 0) {
-                return cb(null, true);
-            } else {
-                return cb(null, false);
-            }
-        });
+        return CardDao.update({
+            where: {id: param.id},
+            data: param.data
+        }, cb);
     }
 };
 

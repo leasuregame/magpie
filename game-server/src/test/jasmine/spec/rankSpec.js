41782e1b619e0c74482c515c50ed3f270636e896
wuzhanghai <wuzhanghai@leasuregame.com>
2013-07-25 17:19:33 +0800

    modify

--- game-server/src/test/jasmine/spec/rankSpec.js
+++ game-server/src/test/jasmine/spec/rankSpec.js
@@ -13,48 +13,48 @@ describe("Ranking List", function() {
 
   });
 
-  // describe("ranking list", function() {
-  //   var ids = [20000, 17000, 15000, 13000, 11000, 10700, 10300, 10199, 10013, 10001];
-  //   var steps = [100, 80, 60, 40, 20, 15, 10, 5, 1, 1];
+  describe("ranking list", function() {
+    var ids = [20000, 17000, 15000, 13000, 11000, 10700, 10300, 10199, 10013, 10001];
+    var steps = [100, 80, 60, 40, 20, 15, 10, 5, 1, 1];
 
-  //   var genRankings = function(rank, stepIndex) {
-  //     var top10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  //     if (rank <= 10) {
-  //       return top10;
-  //     }
-  //     var results = []
-  //     for (var i = 0; i < 10; i++) {
-  //       results.push(rank - steps[stepIndex] * i);
-  //     }
-  //     return _.union(top10, results.reverse());
-  //   };
+    var genRankings = function(rank, stepIndex) {
+      var top10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+      if (rank <= 10) {
+        return top10;
+      }
+      var results = []
+      for (var i = 0; i < 10; i++) {
+        results.push(rank - steps[stepIndex] * i);
+      }
+      return _.union(top10, results.reverse());
+    };
 
-  //   describe("when my ranking grate then 100000", function() {
+    describe("when my ranking grate then 100000", function() {
 
-  //     ids.map(function(id, index) {
+      ids.map(function(id, index) {
 
-  //       it('should can be return correct ranking list for (' + (id - 9999) + ')', function() {
-  //         request('logic.rankHandler.rankingList', {
-  //           playerId: id
-  //         }, function(data) {
-  //           var cur_ranking = id - 9999;
-  //           //expect(data).toEqual('');
-  //           //expect(data.msg[data.msg.length - 1].ranking).toEqual(cur_ranking);
+        it('should can be return correct ranking list for (' + (id - 9999) + ')', function() {
+          request('logic.rankHandler.rankingList', {
+            playerId: id
+          }, function(data) {
+            var cur_ranking = id - 9999;
+            //expect(data).toEqual('');
+            //expect(data.msg[data.msg.length - 1].ranking).toEqual(cur_ranking);
 
-  //           expect(_.map(data.msg, function(p) {
-  //             return p.ranking;
-  //           }).sort(function(a, b) {
-  //             return a - b;
-  //           })).toEqual(genRankings(cur_ranking, index));
-  //           console.log(data);
-  //         });
-  //       });
+            expect(_.map(data.msg, function(p) {
+              return p.ranking;
+            }).sort(function(a, b) {
+              return a - b;
+            })).toEqual(genRankings(cur_ranking, index));
+            console.log(data);
+          });
+        });
 
-  //     });
+      });
 
-  //   });
+    });
 
-  // });
+  });
 
   describe("challenge", function() {
     describe("when one challenge to other", function() {

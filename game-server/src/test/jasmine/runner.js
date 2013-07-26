355723501651d99c99ec460923dc52896a88ec00
wuzhanghai <wuzhanghai@leasuregame.com>
2013-07-26 11:09:09 +0800

    run_test.sh

--- game-server/src/test/jasmine/runner.js
+++ game-server/src/test/jasmine/runner.js
@@ -6,11 +6,11 @@ var client = require('webdriverjs').remote({
     desiredCapabilities: {
         // You may choose other browsers
         // http://code.google.com/p/selenium/wiki/DesiredCapabilities
-        browserName: 'firefox'
+        browserName: 'safari'
     },
     // webdriverjs has a lot of output which is generally useless
     // However, if anything goes wrong, remove this to see more details
-    logLevel: 'silent'
+    // logLevel: 'silent'
 });
  
 client.init()

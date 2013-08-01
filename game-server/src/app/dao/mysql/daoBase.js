72e2005dc6deb42435b4b7e3fc85d2e06e0b5597
wuzhanghai <wuzhanghai@leasuregame.com>
2013-08-01 19:07:05 +0800

    修改，player保存不成功

--- game-server/src/app/dao/mysql/daoBase.js
+++ game-server/src/app/dao/mysql/daoBase.js
@@ -20,7 +20,7 @@ var addSyncEvent = function (syncKey, entity, cb) {
       } else {
         fn = app.get('sync').exec;
       }
-      app.get('sync').flush.call(app.get('sync'), 
+      fn.call(app.get('sync'), 
         syncKey,
         entity.id, 
         {

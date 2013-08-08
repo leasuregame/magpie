fc32b563851b0c308e9a668360b5a8622bd58305
wuzhanghai <wuzhanghai@leasuregame.com>
2013-08-08 16:48:19 +0800

    modify

--- game-server/config/data/starUpgrade.js
+++ game-server/config/data/starUpgrade.js
@@ -4,13 +4,15 @@
 module.exports = {
   STAR_MIN: 3,          // 卡牌星级必须大于此值才能进阶
   STAR_3: {             // 三星进阶
+    money: 10000,
     rate_per_card: 5,   // 每张卡牌提供的进阶概率
     max_num: 20,        // 最多提供的卡牌数量
     gold_per_card: 100  // 代替一张卡牌所需要的元宝数量
   },
   STAR_4: {
+    money: 20000,
     rate_per_card: 3,   // 每张卡牌提供的进阶概率
-    max_num: 20,        // 最多提供的卡牌数量
+    max_num: 34,        // 最多提供的卡牌数量
     gold_per_card: 2000 // 代替一张卡牌所需要的元宝数量
   },
   DEFAULT_INHERIT: {    // 进阶后属性的传承配置信息

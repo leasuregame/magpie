5e7e09de44ca1871ce7b1c3805d3fee7cffe7725
linchenchen <linchenchen@leasuregame.com>
2013-08-22 23:45:46 +0800

    更新配表导出路径
    更新配表导出路径

--- game-server/bin/convertXmlToJson.js
+++ game-server/bin/convertXmlToJson.js
@@ -17,7 +17,6 @@ function tryMkdir(file) {
 
 var files = [
   '../data/table.json',
-  '../../client/Client/Client/Resources/src/table/table.json',
   '../../client/Client-v1.0.0-e2.1.4/Client-v1.0.0-e2.1.4/Resources/src/table/table.json'];
 
 for (var i = files.length - 1; i >= 0; i--) {

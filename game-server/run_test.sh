14d4914a7ddd90ba190cbdd0e8825cbe471988e0
arthur <arthur@arthurtekiMac-mini.local>
2013-09-21 17:23:38 +0800

    modify

--- game-server/run_test.sh
+++ game-server/run_test.sh
@@ -6,10 +6,10 @@ npm install forever -g
 rm -r ./test ./app
 make build
 
-# XUNIT_FILE="test/TESTS-dao.xml" ./node_modules/.bin/mocha -R xunit-file  --require should --recursive ./test/dao
-# XUNIT_FILE="test/TESTS-domain.xml" ./node_modules/.bin/mocha -R xunit-file  --require should --recursive ./test/domain
-# XUNIT_FILE="test/TESTS-battle.xml" ./node_modules/.bin/mocha -R xunit-file  --require should --recursive ./test/battle
-# XUNIT_FILE="test/TESTS-mapping.xml" ./node_modules/.bin/mocha -R xunit-file  --require should --recursive ./test/mapping
+XUNIT_FILE="test/TESTS-dao.xml" ./node_modules/.bin/mocha -R xunit-file  --require should --recursive ./test/dao
+XUNIT_FILE="test/TESTS-domain.xml" ./node_modules/.bin/mocha -R xunit-file  --require should --recursive ./test/domain
+XUNIT_FILE="test/TESTS-battle.xml" ./node_modules/.bin/mocha -R xunit-file  --require should --recursive ./test/battle
+XUNIT_FILE="test/TESTS-mapping.xml" ./node_modules/.bin/mocha -R xunit-file  --require should --recursive ./test/mapping
 
 ./node_modules/.bin/pomelo start development --daemon
 ./node_modules/.bin/forever start ./test/jasmine/app.js

68f691d482124f69bb5af8030c7e05c7f1155edb
arthur <arthur@arthurtekiMac-mini.local>
2013-10-11 10:24:21 +0800

    计算卡牌被动属性加成

--- game-server/src/test/jasmine/spec/area/train/passSkillAfreshSpec.js
+++ game-server/src/test/jasmine/spec/area/train/passSkillAfreshSpec.js
@@ -15,60 +15,64 @@ describe("Area Server", function() {
     });
 
     describe("area.trainHandler.passSkillAfresh", function() {
-      beforeEach(function() {
-        loginWith(arthur.account, arthur.password, arthur.areaId);
-      });
 
-      it("card's pass skill should can be passSkillAfresh", function() {
-        request('area.trainHandler.passSkillAfresh', {
-          playerId: arthur.playerId,
-          cardId: 101,
-          psIds: [0]
-        }, function(data) {
-          expect(data.code).toEqual(200);
+      describe('when money is enought', function() {
+        beforeEach(function() {
+          loginWith(arthur.account, arthur.password, arthur.areaId);
+        });
 
-          doAjax('/card/' + 101, {}, function(res) {
-            console.log(res);
-            var pss = res._data.passiveSkills;
-            expect(pss[0].name).toEqual(data.msg[0].name);
-            expect(pss[0].value).toEqual(data.msg[0].value);
+        it("card's pass skill should can be passSkillAfresh", function() {
+          request('area.trainHandler.passSkillAfresh', {
+            playerId: arthur.playerId,
+            cardId: 101,
+            psIds: [0]
+          }, function(data) {
+            expect(data.code).toEqual(200);
+
+            doAjax('/card/' + 101, {}, function(res) {
+              console.log(res);
+              var pss = res.data.passiveSkills;
+              expect(pss[0].name).toEqual(data.msg[0].name);
+              expect(pss[0].value).toEqual(data.msg[0].value);
+            });
+
+            console.log(data);
           });
-
-          console.log(data);
         });
       });
-    });
 
-    describe("when money is not enought", function() {
-      beforeEach(function() {
-        loginWith('poorman', '1', 1);
-      });
 
-      it("should can not afrash", function() {
-        request('area.trainHandler.passSkillAfresh', {
-          cardId: 164,
-          psIds: [0]
-        }, function(data) {
-          expect(data.code).toEqual(501);
-          expect(data.msg).toEqual('铜板/元宝不足，不能洗炼');
-          console.log(data);
+      describe("when money is not enought", function() {
+        beforeEach(function() {
+          loginWith('poorman', '1', 1);
+        });
+
+        it("should can not afrash", function() {
+          request('area.trainHandler.passSkillAfresh', {
+            cardId: 164,
+            psIds: [0]
+          }, function(data) {
+            expect(data.code).toEqual(501);
+            expect(data.msg).toEqual('铜板/元宝不足，不能洗炼');
+            console.log(data);
+          });
         });
       });
-    });
 
-    describe("when passiveSkill is not exists", function() {
-      beforeEach(function() {
-        loginWith(arthur.account, arthur.password, arthur.areaId);
-      });
+      describe("when passiveSkill is not exists", function() {
+        beforeEach(function() {
+          loginWith(arthur.account, arthur.password, arthur.areaId);
+        });
 
-      it("should can not afrash", function() {
-        request('area.trainHandler.passSkillAfresh', {
-          cardId: 104,
-          psIds: [7]
-        }, function(data) {
-          expect(data.code).toEqual(501);
-          expect(data.msg).toEqual('找不到被动属性');
-          console.log(data);
+        it("should can not afrash", function() {
+          request('area.trainHandler.passSkillAfresh', {
+            cardId: 104,
+            psIds: [7]
+          }, function(data) {
+            expect(data.code).toEqual(501);
+            expect(data.msg).toEqual('找不到被动属性');
+            console.log(data);
+          });
         });
       });
     });

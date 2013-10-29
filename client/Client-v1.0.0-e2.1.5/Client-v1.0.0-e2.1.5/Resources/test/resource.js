888e9a1094d1babfb6ef19b8034683093a27ee3d
bboyonemore <muwan@kimarutekiMac-mini.local>
2013-10-29 11:47:19 +0800

    战斗动画排版

--- client/Client-v1.0.0-e2.1.5/Client-v1.0.0-e2.1.5/Resources/test/resource.js
+++ client/Client-v1.0.0-e2.1.5/Client-v1.0.0-e2.1.5/Resources/test/resource.js
@@ -555,6 +555,7 @@ var main_scene_image = {
     texiao8: ccb_scene_path + "texiao8.ccbi",
     texiao9: ccb_scene_path + "texiao9.ccbi",
     ceshi: ccb_scene_path + "ceshi.ccbi",
+    ceshi2: ccb_scene_path + "ceshi2.ccbi",
     tutorials1: ccb_scene_path + "tutorials1.ccbi",
     tutorials2: ccb_scene_path + "tutorials2.ccbi",
     tutorials3: ccb_scene_path + "tutorials3.ccbi",

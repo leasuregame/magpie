8cfba6cb982e6e1c10448459f23f28cf11d8bf20
linchenchen <linchenchen@leasuregame.com>
2013-11-21 00:26:57 +0800

    change spirit addition progress max value

--- client/Client-v1.0.0-e2.1.5/Client-v1.0.0-e2.1.5/Resources/src/view/battleModule/battleCardNode.js
+++ client/Client-v1.0.0-e2.1.5/Client-v1.0.0-e2.1.5/Resources/src/view/battleModule/battleCardNode.js
@@ -87,7 +87,7 @@ var BattleCardNode = cc.Node.extend({
             null,
             main_scene_image.progress13,
             0,
-            this._hp
+            Math.floor(this._hp / 2)
         );
         this._spiritHpProgress.setPosition(cc.p(0, -100));
         this.addChild(this._spiritHpProgress);
@@ -144,26 +144,26 @@ var BattleCardNode = cc.Node.extend({
                 differenceValue = this._hpProgress.getDifferenceValue();
                 differenceTime = time * differenceValue / absValue;
 
-                this._hpProgress.setAllValue(this._hp, this._hp, differenceTime);
+                this._hpProgress.setValue(this._hp, differenceTime);
 
                 this.scheduleOnce(function () {
-                    this._spiritHpProgress.setAllValue(this._nowHp - this._hp, this._hp, time - differenceTime);
+                    this._spiritHpProgress.setValue(this._nowHp - this._hp, time - differenceTime);
                 }, differenceTime);
             } else {
-                this._hpProgress.setAllValue(this._nowHp, this._hp, time);
+                this._hpProgress.setValue(this._nowHp, time);
             }
         } else if (value < 0) {
             if (this._nowHp < this._hp) {
                 differenceValue = this._spiritHpProgress.getValue();
                 differenceTime = time * differenceValue / absValue;
 
-                this._spiritHpProgress.setAllValue(0, this._hp, differenceTime);
+                this._spiritHpProgress.setValue(0, differenceTime);
 
                 this.scheduleOnce(function () {
-                    this._hpProgress.setAllValue(this._nowHp, this._hp, time - differenceTime);
+                    this._hpProgress.setValue(this._nowHp, time - differenceTime);
                 }, differenceTime);
             } else {
-                this._spiritHpProgress.setAllValue(this._nowHp - this._hp, this._hp, time);
+                this._spiritHpProgress.setValue(this._nowHp - this._hp, time);
             }
         }
     },

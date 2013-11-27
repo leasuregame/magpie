e8bbc3c402b027819666451daf7547b5749ab303
linchenchen <linchenchen@leasuregame.com>
2013-11-27 17:06:03 +0800

    IAPHelp, DataDollection模块

---
+++ client/Client-v1.0.0-e2.1.5/Client-v1.0.0-e2.1.5/libs/IAPHelp/IAPHelp.mm
@@ -0,0 +1,31 @@
+//
+//  IAPHelp.cpp
+//  Client-v1.0.0-e2.1.5
+//
+//  Created by lCeve on 13-11-25.
+//
+//
+
+#include "IAPHelp.h"
+
+CCObject IAPHelp::*target = NULL;
+SEL_CallFuncO IAPHelp::*callback = NULL;
+
+void IAPHelp::buy(const char * productId, CCObject * target, SEL_CallFuncO callback)
+{
+}
+
+void IAPHelp::executeCallback(PaymentState state, const char * product, const char * receipt, const char * msg)
+{
+    if (target && callback)
+    {
+        PaymentData * data = new PaymentData();
+        
+        data->state = state;
+        data->product = product;
+        data->receipt = receipt;
+        data->msg = msg;
+        
+        (target->*callback)((CCObject *)data);
+    }
+}

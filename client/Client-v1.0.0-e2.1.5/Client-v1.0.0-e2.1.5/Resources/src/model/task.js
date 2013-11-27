1b7ff3208c07379f36a66952628331780f651487
lujunyu313 <lujunyu@leasuregame.com>
2013-11-27 20:07:38 +0800

    修复探索卡牌满状态弹购买体力的bug

--- client/Client-v1.0.0-e2.1.5/Client-v1.0.0-e2.1.5/Resources/src/model/task.js
+++ client/Client-v1.0.0-e2.1.5/Client-v1.0.0-e2.1.5/Resources/src/model/task.js
@@ -16,6 +16,10 @@ var TASK_CHAPTER_COUNT = 10;
 var TASK_SECTION_COUNT = 5;
 var TASK_POINTS_COUNT = 10;
 
+var POWER_NO_ENOUGH = 0;
+var CARD_FULL = 1;
+var CAN_EXPLORE = 2;
+
 var Task = Entity.extend({
     _id: 0,
     _progress: 0,
@@ -98,15 +102,15 @@ var Task = Entity.extend({
 
         if (gameData.player.get("power") < this._powerNeed) {
             TipLayer.tip("体力不足");
-            return false;
+            return POWER_NO_ENOUGH;
         }
 
         if (gameData.cardList.isFull()) {
             TipLayer.tip("卡牌已满，请先消耗");
-            return false;
+            return CARD_FULL;
         }
 
-        return true;
+        return CAN_EXPLORE;
     },
 
     canWipeOut: function () {

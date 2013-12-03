4a81e2f981292f1502d573bfc89686c6b02f0505
lujunyu313 <lujunyu@leasuregame.com>
2013-12-03 11:29:04 +0800

    更新gm模拟

--- gm/public/javascripts/models/serverAPI.js
+++ gm/public/javascripts/models/serverAPI.js
@@ -55,13 +55,9 @@ function connectServer(areaId, cb) {
     }, function (data) {
         if (data.code === 200) {
             console.log(data);
-
-            //   dfd.resolve();
         } else {
             console.log(data);
-            //  cb(data.code);
         }
-
         cb(data.code);
 
     });

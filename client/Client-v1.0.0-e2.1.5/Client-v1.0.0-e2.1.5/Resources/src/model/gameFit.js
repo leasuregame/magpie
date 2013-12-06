b4c8f7fc8917a3beed01899205b6bc32eb077734
linchenchen <linchenchen@leasuregame.com>
2013-12-06 12:14:23 +0800

    修改灵气颜色，修改vipLayer特效

--- client/Client-v1.0.0-e2.1.5/Client-v1.0.0-e2.1.5/Resources/src/model/gameFit.js
+++ client/Client-v1.0.0-e2.1.5/Client-v1.0.0-e2.1.5/Resources/src/model/gameFit.js
@@ -1102,12 +1102,12 @@ var fit4Iphone4 = {
             "paymentItem2Point": cc.p(260, 432),
             "closeItemPoint": cc.p(460, 432),
             "vipBoxGoodsPoints": {
-                energy: cc.p(220, 662),
-                money: cc.p(440, 662),
-                skillPoint: cc.p(220, 562),
-                elixir: cc.p(440, 562),
-                fragments: cc.p(220, 462),
-                exp_card: cc.p(440, 462)
+                energy: cc.p(210, 662),
+                money: cc.p(430, 662),
+                skillPoint: cc.p(210, 562),
+                elixir: cc.p(430, 562),
+                fragments: cc.p(210, 462),
+                exp_card: cc.p(430, 462)
             }
         },
         "vipPrivilegeLayer": {
@@ -2223,12 +2223,12 @@ var fit4Iphone5 = {
             "paymentItem2Point": cc.p(220, 520),
             "closeItemPoint": cc.p(420, 520),
             "vipBoxGoodsPoints": {
-                energy: cc.p(180, 750),
-                money: cc.p(400, 750),
-                skillPoint: cc.p(180, 650),
-                elixir: cc.p(400, 650),
-                fragments: cc.p(180, 550),
-                exp_card: cc.p(400, 550)
+                energy: cc.p(170, 750),
+                money: cc.p(390, 750),
+                skillPoint: cc.p(170, 650),
+                elixir: cc.p(390, 650),
+                fragments: cc.p(170, 550),
+                exp_card: cc.p(390, 550)
             }
         },
         "vipPrivilegeLayer": {

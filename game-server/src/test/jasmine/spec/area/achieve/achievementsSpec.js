2804ca027f3a12225e01dccea6a00ef1e0ff4a73
wuzhanghai <wuzhanghai@leasuregame.com>
2013-12-21 16:03:49 +0800

    修改领取成就奖励和测试

--- game-server/src/test/jasmine/spec/area/achieve/achievementsSpec.js
+++ game-server/src/test/jasmine/spec/area/achieve/achievementsSpec.js
@@ -195,14 +195,22 @@ describe("Area Server", function() {
 				});
 
 				describe('when player pass layer is 100', function() {
+					var before_player;
+
 					beforeAll(function() {
 						doAjax('/loaddata/csv', {}, function() {});
 					});
 					beforeEach(function() {
 						doAjax('/update/player/100', {
-							passLayer: 100
+							passLayer: 100,
+							energy: 1000,
+							gold: 1000
 						}, function(res) {
-							loginWith('arthur', '1', 1);
+							doAjax('/player/100', function(res){
+								before_player = res.data;
+								loginWith('arthur', '1', 1);
+							});
+							
 						});
 					});
 
@@ -227,6 +235,11 @@ describe("Area Server", function() {
 						}, function(data) {
 							expect(data).toEqual({
 								code: 200
+							});
+
+							doAjax('/player/100', function(res) {
+								expect(res.data.energy).toEqual(before_player.energy+1000);
+								expect(res.data.gold).toEqual(before_player.gold+100);
 							});
 						});
 

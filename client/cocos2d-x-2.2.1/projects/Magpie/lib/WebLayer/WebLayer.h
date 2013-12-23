91fcc07a46fcea7f5927e716f7e51fc85bfb4b15
linchenchen <linchenchen@leasuregame.com>
2013-12-23 14:15:57 +0800

    绑定网页控件模块

--- client/cocos2d-x-2.2.1/projects/Magpie/lib/WebLayer/WebLayer.h
+++ client/cocos2d-x-2.2.1/projects/Magpie/lib/WebLayer/WebLayer.h
@@ -9,19 +9,24 @@
 #ifndef __WEB_LAYER__
 #define __WEB_LAYER__
 
+#ifdef __OBJC__
+@class WebLayerOC;
+#else
+typedef struct objc_object WebLayerOC;
+#endif
+
 #include "cocos2d.h"
-#import "WebLayerOC.h"
 
 USING_NS_CC;
 
 class WebLayer
 {
 public:
-    WebLayer(const char * url, const CCRect * rect);
+    WebLayer(const char * url, const CCRect rect);
     virtual ~WebLayer();
     void close();
     
-    static WebLayer * create(const char * url, const CCRect * rect);
+    static WebLayer * create(const char * url, const CCRect rect);
     
 private:
     WebLayerOC * _webLayerOC;

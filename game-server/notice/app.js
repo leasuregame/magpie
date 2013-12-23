faf376f5d1ddded6768048fa85d9cb58e14870b2
wuzhanghai <wuzhanghai@leasuregame.com>
2013-12-23 20:12:55 +0800

    修改公告

--- game-server/notice/app.js
+++ game-server/notice/app.js
@@ -3,9 +3,12 @@ var url = require('url');
 var path = require('path');
 var read = require('fs').readFileSync;
 var ejs = require('ejs');
+var htmlStr = require('html-strings');
 
 var HOST = '127.0.0.1';
-var PORT = '6000'
+var PORT = '8001'
+
+var notice_path = './static/notice.html'
 
 function readFile(name) {
   return read(path.join(__dirname, name), 'utf8').replace(/\r/g, '');
@@ -13,20 +16,57 @@ function readFile(name) {
 
 http.createServer(function(req, res) {
 	var pathname = url.parse(req.url).pathname;
-	console.log(pathname, pathname != '/notice', pathname != '/notice/');
-	if (req.method != 'GET') {
-		res.writeHead(401, 'Forbidden')
-		return res.end();
-	}
 
-	if (pathname != '/notice' && pathname != '/notice/'){
+	if (!pathname in routes){
 		res.writeHead(404, 'Not Found');
 		return res.end();
 	}
+	
+	if (/^\/static\//.test(pathname)) {
+		pathname = '/static';
+	}
 
-	var html = ejs.render(readFile('index.html'), {text: 'notice content'});
-	res.write(html);
-	res.end();
-}).listen(8001);
+	return routes[pathname](req, res);
+}).listen(PORT, HOST);
 
 console.log('notice server runing on http:'+HOST+':'+PORT);
+
+var index = function(req, res) {
+	var html = ejs.render(readFile('index.html'), {
+		text: 'notice content'
+	});
+	res.write(html);
+	res.end();
+};
+
+var admin = function(req, res) {
+	res.write(ejs.render(readFile('/static/admin.ejs'), {
+		text: htmlStr.escape(readFile('/static/notice.ejs')),
+		filename: path.join(__dirname, 'static', 'admin.ejs')
+	}));
+	res.end();
+};
+
+var saveNotice = function(req, res) {
+	if (req.method != "POST") {
+		res.writeHead(401, 'Forbidden')
+		return res.end();
+	}
+	console.log(req);
+
+	//fs.writeFileSync(notice_path, text);
+	res.end();
+};
+
+var static = function(req, res) {
+	var filename = url.parse(req.url).pathname;
+	res.write(ejs.render(readFile(filename)));
+	res.end();
+};
+
+var routes = {
+	'/notice': index,
+	'/admin': admin,
+	'/notice/save': saveNotice,
+	'/static': static
+};

179a57d29db2b8ed051a85ee6213e3ba10f07b03
linchenchen <linchenchen@leasuregame.com>
2014-02-13 18:47:12 +0800

    重构代码

--- client/cocos2d-x-2.2.1/projects/Magpie/Resources/src/model/event.js
+++ client/cocos2d-x-2.2.1/projects/Magpie/Resources/src/model/event.js
@@ -64,7 +64,7 @@ var Event = cc.Class.extend({
     emit: function (event) {
         this._callbacks = this._callbacks || {};
         var args = [].slice.call(arguments, 1)
-            , callbacks = this._callbacks[event];
+        var callbacks = this._callbacks[event];
 
         if (callbacks) {
             callbacks = callbacks.slice(0);

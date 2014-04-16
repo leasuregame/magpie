adf09a3ccb8646e8e74982b99f5817f8a2b7c390
wuzhanghai <wuzhanghai@leasuregame.com>
2014-04-16 16:29:27 +0800

    修改创建数据库命令

--- game-server/bin/initMysql.sh
+++ game-server/bin/initMysql.sh
@@ -1,6 +1,6 @@
 db=$1
 
 mysql -uroot -pleasure:GAME -e "drop database if exists $db;create database $db;use $db;source ./config/schema/init.sql;GRANT ALL PRIVILEGES ON $db.* to yy_leasuregame@localhost IDENTIFIED BY 'leasure405GAME';" 
-mysql -uroot -pleasure:GAME -e "drop database if exists userdb1;create database userdb1;use userdb1;source ./config/schema/init.sql;GRANT ALL PRIVILEGES ON userdb1.* to yy_leasuregame@localhost IDENTIFIED BY 'leasure405GAME';"
+mysql -uroot -pleasure:GAME -e "drop database if exists yy_userdb;create database yy_userdb;use yy_userdb;source ./config/schema/init.sql;GRANT ALL PRIVILEGES ON yy_userdb.* to yy_leasuregame@localhost IDENTIFIED BY 'leasure405GAME';"
 mysql -uroot -pleasure:GAME -e "use yy_userdb;source ./config/schema/userdb.sql;"
 mysql -uroot -pleasure:GAME -e "use $db;source ./config/schema/magpiedb.sql;"

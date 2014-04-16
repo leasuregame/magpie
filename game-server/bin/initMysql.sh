bdbbcc54ad235f48bc52ee796d05a1533e79b484
wuzhanghai <wuzhanghai@leasuregame.com>
2014-04-16 16:26:32 +0800

    修改初始化数据库命令for yy平台

--- game-server/bin/initMysql.sh
+++ game-server/bin/initMysql.sh
@@ -1,6 +1,6 @@
 db=$1
 
-mysql -uroot -p1 -e "drop database if exists $db;create database $db;use $db;source ./config/schema/init.sql;GRANT ALL PRIVILEGES ON $db.* to dev@localhost IDENTIFIED BY '1';" 
-mysql -uroot -p1 -e "drop database if exists userdb1;create database userdb1;use userdb1;source ./config/schema/init.sql;GRANT ALL PRIVILEGES ON userdb1.* to dev@localhost IDENTIFIED BY '1';"
-mysql -uroot -p1 -e "use userdb1;source ./config/schema/userdb.sql;"
-mysql -uroot -p1 -e "use $db;source ./config/schema/magpiedb.sql;"
+mysql -uroot -pleasure:GAME -e "drop database if exists $db;create database $db;use $db;source ./config/schema/init.sql;GRANT ALL PRIVILEGES ON $db.* to yy_leasuregame@localhost IDENTIFIED BY 'leasure405GAME';" 
+mysql -uroot -pleasure:GAME -e "drop database if exists userdb1;create database userdb1;use userdb1;source ./config/schema/init.sql;GRANT ALL PRIVILEGES ON userdb1.* to yy_leasuregame@localhost IDENTIFIED BY 'leasure405GAME';"
+mysql -uroot -pleasure:GAME -e "use yy_userdb;source ./config/schema/userdb.sql;"
+mysql -uroot -pleasure:GAME -e "use $db;source ./config/schema/magpiedb.sql;"

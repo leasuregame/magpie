1b0d1c052fab6f7dd49f3916dae7fafa82c4a81b
wuzhanghai <wuzhanghai@leasuregame.com>
2014-04-18 11:04:32 +0800

    修改获取服务器列表参数

--- gm/public/javascripts/models/serverAPI.js
+++ gm/public/javascripts/models/serverAPI.js
@@ -20,7 +20,10 @@ function initServer(cb) {
     }, function () {
 
         var route = "gate.gateHandler.queryEntry";
-        pomelo.request(route, {}, function (data) {
+        pomelo.request(route, {
+            os: 'IOS',
+            platform: 'YY'
+        }, function (data) {
             console.log(data);
             var msg = data.msg;
             port = msg.port;

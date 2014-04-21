db33869c5b9b98e21646ebc692f0ac74c3a75ee0
linchenchen <lcc3536@163.com>
2014-04-21 16:01:25 +0800

    fix bug

--- client/cocos2d-x-2.2.1/projects/Magpie/Resources/res/resource.js
+++ client/cocos2d-x-2.2.1/projects/Magpie/Resources/res/resource.js
@@ -1302,7 +1302,7 @@ var main_scene_image = {
     boss2_png: "res/effect/effect/boss2.png",
     boss2_plist: "res/effect/effect/boss2.plist",
     boss3_png: "res/effect/effect/boss3.png",
-    boss3_plist: "res/effect/effect/boss3.plist",
+    boss3_plist: "res/effect/effect/boss3.plist"
 };
 
 var main_scene_res = [

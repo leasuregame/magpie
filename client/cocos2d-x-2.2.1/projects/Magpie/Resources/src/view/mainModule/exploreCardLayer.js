37691b631297c2b7ad55466ff812bd9b9a7996a2
lujunyu313 <lujunyu@leasuregame.com>
2014-05-09 11:19:19 +0800

    修改探索卡牌掉落特效

--- client/cocos2d-x-2.2.1/projects/Magpie/Resources/src/view/mainModule/exploreCardLayer.js
+++ client/cocos2d-x-2.2.1/projects/Magpie/Resources/src/view/mainModule/exploreCardLayer.js
@@ -7,6 +7,7 @@ var ExploreCardLayer = LazyLayer.extend({
     _canClick: false,
     _ccbNode: null,
     _cb: null,
+    _card: null,
 
     onEnter: function () {
         cc.log("ExploreCardLayer onEnter");
@@ -29,20 +30,24 @@ var ExploreCardLayer = LazyLayer.extend({
 
         this.setTouchPriority(MAIN_MENU_LAYER_HANDLER_PRIORITY);
 
-        var card = data.card;
+        this._card = data.card;
         this._cb = data.cb;
         this._canClick = false;
 
         this._ccbNode = cc.BuilderReader.load(main_scene_image.uiEffect110, this);
         this._ccbNode.setPosition(gameFit.GAME_MIDPOINT);
 
-        var url = card.get("url");
-        var star = card.get("star");
+        var controller = this._ccbNode.controller;
+
+        controller.ccbNameLabel.setString(this._card.get("name"));
+
+        var url = this._card.get("url");
+        var star = this._card.get("star");
         var index = star > 2 ? star - 2 : 1;
 
         this["ccbCardFrame1"].setTexture(lz.getTexture(main_scene_image["card_frame" + star]));
         this["ccbCardHalf1"].setTexture(lz.getTexture(main_scene_image[url + "_half" + index]));
-        this["ccbCardIcon1"].setTexture(lz.getTexture(card.getCardIcon()));
+        this["ccbCardIcon1"].setTexture(lz.getTexture(this._card.getCardIcon()));
 
         this.addChild(this._ccbNode);
 
@@ -51,6 +56,33 @@ var ExploreCardLayer = LazyLayer.extend({
         });
 
         return true;
+    },
+
+    ccbFnStartSetStar: function() {
+        cc.log("ExploreCardLayer ccbFnStartSetStar");
+
+        this._index = 0;
+
+        this._setStar();
+    },
+
+    _setStar: function () {
+        cc.log("ExploreCardLayer _setStar");
+
+        var star = this._card.get("star");
+        var offset = 60;
+
+        if (this._index < star) {
+            var starNode = cc.BuilderReader.load(main_scene_image.uiEffect31, this);
+            starNode.setPosition(cc.p(this._index * offset - offset * (star - 1) / 2, -200));
+            this._ccbNode.addChild(starNode);
+
+            starNode.animationManager.setCompletedAnimationCallback(this, function () {
+                this._setStar();
+            });
+
+            this._index += 1;
+        }
     },
 
     onTouchBegan: function (touch, event) {

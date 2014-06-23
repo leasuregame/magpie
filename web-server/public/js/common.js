2638982001cd0fbb2e1a475166508c7b2deced2f
nalinai1125 <nalinai1125@qq.com>
2014-06-23 18:48:04 +0800

    修改页面导航,增加根据host自适应平台名称

---
+++ web-server/public/js/common.js
@@ -0,0 +1,19 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: xiejiayue
+ * To change this template use File | Settings | File Templates.
+ */
+
+$(document).ready(function() {
+
+    function initPlatformName(){
+        var ipPlatfomNamePair = {
+          "115.29.12.178:9090" : "YY大神",
+          "125.90.93.74:9090" : "越狱平台",
+          "115.29.12.80:9090" : "AppStore平台"
+        };
+        $('#platformName span').text(ipPlatfomNamePair[window.location.host]);
+    }
+
+    initPlatformName();
+});

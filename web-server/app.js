f45c2faccd311b1b6236e746ea7f4457f42e4129
wuzhanghai <wuzhanghai@leasuregame.com>
2014-07-02 17:33:04 +0800

    Revert 88f7fed..93c4f62
    This rolls back to commit 88f7fed0cd41d3d9b5bad99a83bcf7d608fd43a5.
    
    Change-Id: I67df989ed81aec1f8475dd88b18efd7aa2cbbf46

--- web-server/app.js
+++ web-server/app.js
@@ -24,7 +24,6 @@ var optRecord = require('./routes/optRecord');
 var playerRecord = require('./routes/playerRecord');
 var upload = require('./routes/upload');
 var serverManager = require('./routes/serverManager');
-var jsonEditor = require('./routes/jsonEditor');
 
 var app = express();
 
@@ -67,7 +66,6 @@ optRecord(app);
 playerRecord(app);
 upload(app);
 serverManager(app);
-jsonEditor(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));

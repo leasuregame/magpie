dd47478ddeb489464383d920c7e923fcb3322f5a
wuzhanghai <wuzhanghai@leasuregame.com>
2014-07-12 17:03:47 +0800

    更新世界杯比赛
    Change-Id: I164e835b222e74b76af6ac3a76e07ed890251569

--- game-server/bin/backupdb.sh
+++ game-server/bin/backupdb.sh
@@ -2,6 +2,4 @@ curTime=`date "+%Y-%m-%d_%H:%M:%S"`
 filepath="/data/mysql-backup/dbbackup_$curTime.tar.gz"
 echo $filepath
 
-sudo -i
-
 innobackupex --user=root --password=leasure:GAME --defaults-file=/etc/mysql/my.cnf --stream=tar /data/mysql-backup | gzip -> $filepath
